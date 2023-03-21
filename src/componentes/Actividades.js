40fe286d6c8be6a7a21db23a5ba063296120725f
Jossu0803 <josue0803jcl@gmail.com>
2023-03-20 23:06:45 -0400

    Co-authored-by: lianmcvg@gmail.com <lianmcvg@gmail.com> Co-authored-by: jor.g.t97@gmail.com <jor.g.t97@gmail.com>

--- src/componentes/Actividades.js
+++ src/componentes/Actividades.js
@@ -7,31 +7,69 @@ function Actividades() {
     <div className="contenedor contenido-SectionActividades">
         <h3 style={{textAlign: "center"}}>Evento X Actividades</h3>
         <div className="actividades">
-            <table className="actividad">
-                <tbody>
-                    <tr>
-                        <td>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checkbox" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <polyline points="9 11 12 14 20 6" />
-                            <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
-                            </svg>
-                        </td>
-                        <td>
-                            <div>
-                               <p>08:00 - 10:00</p> 
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <p>El programa de escritura de actividades CS POGIL</p>
-                                <p>Expositor: Pepe</p>
-                                <p>Material: </p>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="actividad">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checkbox" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7bc62d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <polyline points="9 11 12 14 20 6" />
+                    <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+                    </svg>
+                </div>
+                <div>
+                    <div>
+                        <p>08:00 - 09:50</p> 
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p>El programa de escritura de actividades CS POGIL</p>
+                        <p>Expositor: Pepe</p>
+                        <p>Material: </p>
+                    </div>
+                </div>
+            </div>
+            <div className="actividad">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checkbox" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#7bc62d" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <polyline points="9 11 12 14 20 6" />
+                    <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+                    </svg>
+                </div>
+                <div>
+                    <div>
+                        <p>10:00 - 11:50</p> 
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p>El programa de escritura de actividades CS POGIL</p>
+                        <p>Expositor: Pepe</p>
+                        <p>Material: </p>
+                    </div>
+                </div>
+            </div>
+            <div className="actividad">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-checkbox" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <polyline points="9 11 12 14 20 6" />
+                    <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+                    </svg>
+                </div>
+                <div>
+                    <div>
+                        <p>12:00 - 13:50</p> 
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p>El programa de escritura de actividades CS POGIL</p>
+                        <p>Expositor: Pepe</p>
+                        <p>Material: </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   );
---
+++ src/componentes/Participante.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Partipante() {
+  return (
+    <div>
+      <header>
+        <a href="/Partipante">Inicio</a>
+        <a href="/Partipante">Certificados</a>
+      </header>
+      <body>
+        <h1>Hola, persona X</h1>
+        <div>
+          
+        </div>
+      </body>
+
+
+
+    </div>
+    
+    
+  );
+}
+export default Partipante;
